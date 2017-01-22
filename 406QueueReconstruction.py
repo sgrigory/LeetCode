a8e300657274602241f20701505a31a38c55ccbb
sgrigory <grisha.sizov@gmail.com>
2017-01-22 16:16:40 +0100

    accepted

---
+++ 406QueueReconstruction.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def reconstructQueue(self, people):
+        """
+        :type people: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        lst=[]
+        people=sorted(people,key=lambda x: (-x[0],x[1]))
+        for x in people:
+            lst=lst[:x[1]]+[x]+lst[x[1]:]
+        return(lst)
